d1703228acf693ea8b2975daaaea64237add133e
李虎 -。= <lhu_job@163.com>
2019-08-30 18:10:54 +0800

    feat: 学习 练习

---
+++ com/lihu/test/HelloWorld.py
@@ -0,0 +1,14 @@
+from pip._vendor.distlib.compat import raw_input
+
+print("hello,word")
+print("你好世界！")
+
+if 111 == 111:
+    print("123")
+# 这是注释
+string = "1234" + \
+         "344555" + \
+    "45156646" + "\n张三"
+print(string)\
+
+raw_input("按下 enter 键退出，其他任意键显示...\n")
